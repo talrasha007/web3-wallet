42cbd942d76112f3b3ac4edb98c9519c5034b787
ttl <talrasha007@gmail.com>
2018-03-13 17:13:18 +0800

    promisify eth api.

--- index.js
+++ index.js
@@ -10,6 +10,20 @@ const RpcSubprovider = require('web3-provider-engine/subproviders/rpc.js');
 //Web3 Module
 const Web3 = require('web3');
 
+function promisify(fn) {
+    return function () {
+        const args = Array.prototype.slice.call(arguments, 0);
+        return new Promise(function (resolve, reject) {
+            args.push(function (err, res) {
+                if (err) reject(err);
+                else resolve(res);
+            });
+
+            fn.apply(null, args);
+        });
+    }
+}
+
 function fromPrivateKey(privateKey) {
     //Wallet Initialization
     const privateKeyBuffer = new Buffer(privateKey, 'hex');
@@ -68,7 +82,27 @@ function create(myWallet, rpcUrl) {
     engine.start();
 
     //Actual Initialization of the web3 module
-    return new Web3(engine);
+    const web3 = new Web3(engine);
+
+    web3.eth.defaultAccount = myWallet.getAddressString();
+    web3.eth.getBalance = promisify(web3.eth.getBalance);
+    web3.eth.getBlockNumber = promisify(web3.eth.getBlockNumber);
+    web3.eth.getBlock = promisify(web3.eth.getBlock);
+    web3.eth.getTransactionReceipt = promisify(web3.eth.getTransactionReceipt);
+
+    web3.eth.loadContract = function (abi, address) {
+      const instance = web3.eth.contract(abi).at(address);
+
+      abi.forEach(api => {
+        if (api.type === 'function') {
+          instance[api.name] = promisify(instance[api.name]);
+        }
+      });
+
+      return instance;
+    };
+
+    return web3;
 }
 
 module.exports = {
